a3fe1d8c99b5cb43652df8a2ec63f01a2c2f8f02
richcire <eric9907@naver.com>
2022-09-15 21:01:58 +0900

    complete header design & main page layout design

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { IoPersonOutline } from "react-icons/io5";
 
 const NavigatorContainer = styled.header`
   width: 100%;
   height: 80px;
-  background-color: beige;
+  background-color: whitesmoke;
 `;
 
 const ItemsContainer = styled.div`
   margin: 0 auto;
-  background-color: bisque;
+  /* background-color: bisque; */
   display: flex;
   height: 100%;
   max-width: 1200px;
@@ -18,7 +19,7 @@ const ItemsContainer = styled.div`
 const LogoContainer = styled(Link)`
   height: 100%;
   width: 200px;
-  background-color: aliceblue;
+  /* background-color: aliceblue; */
   text-decoration: none;
   display: flex;
   align-items: center;
@@ -26,8 +27,36 @@ const LogoContainer = styled(Link)`
 `;
 
 const Logo = styled.div`
-  color: blueviolet;
+  color: black;
   font-size: 3rem;
+`;
+
+const NavigationContainer = styled.nav`
+  margin-left: 40px;
+`;
+
+const NavigationUl = styled.ul`
+  display: flex;
+  height: 100%;
+  gap: 40px;
+`;
+
+const NavigationLi = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NavigationLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-size: 2rem;
+`;
+
+const LoginBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  margin-left: auto;
 `;
 
 function Navigator() {
@@ -37,6 +66,22 @@ function Navigator() {
         <LogoContainer to="/">
           <Logo>SKKUDO</Logo>
         </LogoContainer>
+        <NavigationContainer>
+          <NavigationUl>
+            <NavigationLi>
+              <NavigationLink to="/clubs">Club</NavigationLink>
+            </NavigationLi>
+            <NavigationLi>
+              <NavigationLink to="/about">About</NavigationLink>
+            </NavigationLi>
+            <NavigationLi>
+              <NavigationLink to="/myPage">My Page</NavigationLink>
+            </NavigationLi>
+          </NavigationUl>
+        </NavigationContainer>
+        <LoginBtn>
+          <IoPersonOutline size="2.3rem" />
+        </LoginBtn>
       </ItemsContainer>
     </NavigatorContainer>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,80 @@
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Banner = styled.div`
+  background-color: #dfc6a9;
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+`;
+
+const LineOne = styled.div`
+  font-size: 4.5rem;
+`;
+
+const LineTwo = styled.div`
+  font-size: 4.5rem;
+`;
+
+const Name = styled.div`
+  font-size: 6.5rem;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 150px;
+  gap: 80px;
+`;
+
+const MainPageBtn = styled.button`
+  width: 150px;
+  height: 80px;
+  background-color: transparent;
+  border-radius: 5px;
+  font-size: 1rem;
+`;
+
+// const ButtonLink = styled(Link)`
+//   text-decoration: none;
+//   color: black;
+//   font-size: 1rem;
+//   width: 100%;
+//   height: 100%;
+// `;
+
 function HomePage() {
-  return <div>Home page</div>;
+  const navigate = useNavigate();
+  const handleMainPageBtnClick = (btnType: string) => {
+    if (btnType == "search") {
+      navigate("/clubs");
+    } else if (btnType == "make") {
+      navigate("/about");
+    } else {
+      return;
+    }
+  };
+  return (
+    <>
+      <Banner>
+        <LineOne>동아리/학회 관리를</LineOne>
+        <LineTwo>손쉽게!!</LineTwo>
+        <Name>SKKUDO</Name>
+      </Banner>
+      <ButtonsContainer>
+        <MainPageBtn onClick={() => handleMainPageBtnClick("search")}>
+          동아리/학회 둘러보기
+        </MainPageBtn>
+        <MainPageBtn onClick={() => handleMainPageBtnClick("make")}>
+          동아리 신청하기
+        </MainPageBtn>
+      </ButtonsContainer>
+    </>
+  );
 }
 
 export default HomePage;
