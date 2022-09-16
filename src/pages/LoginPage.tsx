02c72996db722699dfcb1ca7901162ecbb75c77b
richcire <eric9907@naver.com>
2022-09-16 15:25:58 +0900

    make layout of login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const LoginPageContainer = styled.div`
@@ -7,9 +9,20 @@ const LoginPageContainer = styled.div`
   background-color: aliceblue;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-size: 3rem;
+  margin-bottom: 25px;
 `;
 
 const LoginForm = styled.form`
+  display: flex;
+  gap: 20px;
+`;
+
+const LoginInputContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -17,12 +30,36 @@ const LoginForm = styled.form`
 
 const LoginInput = styled.input``;
 
+const LoginButton = styled.button``;
+
 function LoginPage() {
+  const navigate = useNavigate();
+  const [ID, setID] = useState("");
+  const [PW, setPW] = useState("");
+
+  const handleIDChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setID(event.target.value);
+  };
+
+  const handlePWChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPW(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(ID, PW);
+    navigate("/myPage");
+  };
+
   return (
     <LoginPageContainer>
-      <LoginForm>
-        <LoginInput></LoginInput>
-        <LoginInput></LoginInput>
+      <Title>SKKUDO</Title>
+      <LoginForm onSubmit={handleSubmit}>
+        <LoginInputContainer>
+          <LoginInput onChange={handleIDChange}></LoginInput>
+          <LoginInput type="password" onChange={handlePWChange}></LoginInput>
+        </LoginInputContainer>
+        <LoginButton type="submit">Log In</LoginButton>
       </LoginForm>
     </LoginPageContainer>
   );
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+
+const MyPageContainer = styled.div`
+  padding-top: 80px;
+`;
+
+const SectionContainer = styled.div`
+  margin-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  max-width: 1024px;
+`;
+
+const ClubCardsContainer = styled.div``;
+
 function MyPage() {
-  return <div>My page</div>;
+  return (
+    <MyPageContainer>
+      <SectionContainer>
+        <Title>내 동아리</Title>
+        <ClubCardsContainer></ClubCardsContainer>
+      </SectionContainer>
+    </MyPageContainer>
+  );
 }
 
 export default MyPage;
