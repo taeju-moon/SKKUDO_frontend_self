dfa02302cd9bb44a2a8005a5de98d251ac9cbb34
richcire <eric9907@naver.com>
2022-09-18 16:45:22 +0900

    build basic layout of calendar page

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,5 +1,64 @@
+import styled from "styled-components";
+import Calendar from "react-calendar";
+import React, { useState } from "react";
+import "react-calendar/dist/Calendar.css";
+
+const CalendarContainer = styled.div`
+  padding-top: 80px;
+  display: flex;
+  justify-content: center;
+`;
+
+interface IDayDetailOverlay {
+  isDayDetailOpened: boolean;
+}
+const DayDetailOverlay = styled.div<IDayDetailOverlay>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: ${(props) => (props.isDayDetailOpened ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+`;
+
+const DayDetailBoard = styled.div<IDayDetailOverlay>`
+  position: fixed;
+  top: 10%;
+  transform: translateX(-50%);
+  left: 50%;
+  width: 100%;
+  max-width: 1024px;
+  height: 800px;
+  background-color: aliceblue;
+  display: ${(props) => (props.isDayDetailOpened ? "flex" : "none")};
+`;
+
 function CalendarPage() {
-  return <div>Calendar</div>;
+  const [value, onChange] = useState(new Date());
+  const handleDayClick = () => {
+    setIsDayDetailOpened((prev) => !prev);
+  };
+
+  const [isDayDetailOpened, setIsDayDetailOpened] = useState(false);
+  return (
+    <>
+      <CalendarContainer>
+        <Calendar
+          onChange={onChange}
+          value={value}
+          onClickDay={handleDayClick}
+        />
+      </CalendarContainer>
+      <DayDetailOverlay
+        isDayDetailOpened={isDayDetailOpened}
+        onClick={handleDayClick}
+      ></DayDetailOverlay>
+      <DayDetailBoard isDayDetailOpened={isDayDetailOpened}></DayDetailBoard>
+    </>
+  );
 }
 
 export default CalendarPage;
