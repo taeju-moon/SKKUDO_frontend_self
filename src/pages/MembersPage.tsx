6415b887dc8e66e7361513f9963d3a661a6f7fe6
richcire <eric9907@naver.com>
2022-09-18 17:50:46 +0900

    add example table to members page

--- src/pages/MembersPage.tsx
+++ src/pages/MembersPage.tsx
@@ -1,5 +1,80 @@
+import styled from "styled-components";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+function BasicTable() {
+  return (
+    <TableContainer
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        border: "none",
+        boxShadow: "none",
+        marginTop: "40px",
+      }}
+      component={Paper}
+    >
+      <Table sx={{ maxWidth: 1024, width: "100%" }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>이름</TableCell>
+            <TableCell align="right">학번</TableCell>
+            <TableCell align="right">권한</TableCell>
+            <TableCell align="right">학과</TableCell>
+            <TableCell align="right">기타</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+const MembersPageContainer = styled.div`
+  padding-top: 80px;
+`;
 function MembersPage() {
-  return <div>Members</div>;
+  return (
+    <MembersPageContainer>
+      <BasicTable />
+    </MembersPageContainer>
+  );
 }
 
 export default MembersPage;
