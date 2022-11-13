e88ad1d3b3d82b278f45b4c0959952761d5ce54e
richcire <eric9907@naver.com>
2022-11-13 23:18:31 +0900

    show name&role in profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { loggedInUserState } from "../atoms/userAtom";
+import ClubDetailHeader from "../components/ClubDetailHeader";
+import { RegisteredClubType } from "../types/user";
+
+const ProfileContainer = styled.div``;
+
+const Name = styled.div``;
+const Role = styled.div``;
+
 function ProfilePage() {
-  return <div></div>;
+  const { clubID } = useParams();
+  const loggedInUser = useRecoilValue(loggedInUserState);
+  const [clubProfile, setClubProfile] = useState<RegisteredClubType>();
+
+  useEffect(() => {
+    if (loggedInUser) {
+      const registedClubs = new Map(
+        Object.entries(loggedInUser.registeredClubs)
+      );
+      setClubProfile(registedClubs.get(clubID || ""));
+    }
+  }, [loggedInUser]);
+  return (
+    <>
+      <ClubDetailHeader pageType="내 프로필" />
+      <ProfileContainer>
+        <Name>{loggedInUser?.name}</Name>
+        <Role>{clubProfile?.role}</Role>
+      </ProfileContainer>
+    </>
+  );
 }
 
 export default ProfilePage;
