a5a39023d7cdbae9857f3cb14c9fad844408cae9
richcire <eric9907@naver.com>
2022-11-14 11:16:04 +0900

    fix profile page

---
+++ src/components/profileComponents/ColumnDialog.tsx
@@ -0,0 +1,33 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
+
+function ColumnDialog() {
+  //   return <Dialog open={dialogOpen} onClose={handleClose}>
+  //   <DialogTitle>{`${clubUpdate.keyword} 변경`}</DialogTitle>
+  //   <>
+  //         <DialogContent style={{ width: "512px" }}>
+  //           <TextField
+  //             autoFocus
+  //             margin="dense"
+  //             id="name"
+  //             label="새로운 설정"
+  //             fullWidth
+  //             variant="standard"
+  //             value={name}
+  //             sx={{ marginTop: "20px" }}
+  //             onChange={handleNameChange}
+  //           />
+  //         </DialogContent>
+  //         <DialogActions>
+  //           <Button onClick={handleClose}>취소</Button>
+  //           <Button onClick={handleUpdateSubmit}>수정</Button>
+  //         </DialogActions>
+  //       </>
+  // </Dialog>
+  return <div></div>;
+}
