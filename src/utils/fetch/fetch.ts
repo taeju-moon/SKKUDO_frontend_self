880e37fb09171b65a013ce5185a9c884c0a6328e
richcire <eric9907@naver.com>
2023-01-01 15:28:21 +0900

    Feat: change base URL for fetch

--- src/utils/fetch/fetch.ts
+++ src/utils/fetch/fetch.ts
@@ -4,5 +4,5 @@ axios.defaults.withCredentials = true;
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "http://api.skkudo.link"
+    ? "https://api.skkudo.link"
     : "http://localhost:8000";
